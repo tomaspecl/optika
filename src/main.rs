562e1088613301694b54383f778a924f900edb75
tomaspecl <tomaspecl@email.cz>
2021-06-27 22:57:11 +0200

    Made traits more general

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,8 @@ pub trait Parametric<T> where T: Copy + num_traits::Num {
 }
 
 pub trait Intersect<O,T> where T: Copy + num_traits::Num {
-    fn intersections(&self, object: &O) -> Box<[Point3D<T,UnknownUnit>]>;
+    type Intersection;
+    fn intersections(&self, object: &O) -> Box<[Self::Intersection]>;
 }
 
 pub trait Surface<T> where T: Copy + num_traits::Num {
@@ -51,20 +52,11 @@ pub trait ParametricSurface<T>: Parametric<T> where T: Copy + num_traits::Num {
     fn normal(&self, parameter: Self::Parameter) -> Vector3D<T,UnknownUnit>;
 }
 
-pub trait Ray<T> where T: Copy + num_traits::Num {
-    fn start(&self) -> Point3D<T,UnknownUnit>;
-    fn direction(&self) -> Vector3D<T,UnknownUnit>;
+pub trait Reflect<T>: Intersect<Line<T>,T> where T: Copy + num_traits::Num {
+    fn reflect(&self, line: &Line<T>) -> Option<Line<T>>;
 }
 
 impl<T> Parametric<T> for Line<T> where T: Copy + num_traits::Num {
-    type Parameter=T;
-
-    fn point(&self, parameter: Self::Parameter) -> Point3D<T,UnknownUnit> {
-        self.start+self.direction*parameter
-    }
-}
-
-impl<T> Parametric<T> for HalfLine<T> where T: Copy + num_traits::Num {
     type Parameter=T;
 
     fn point(&self, parameter: Self::Parameter) -> Point3D<T,UnknownUnit> {
@@ -78,29 +70,11 @@ impl<T> Surface<T> for Ball<T> where T: Copy + num_traits::Num {
     }
 }
 
-impl<T> Ray<T> for Line<T> where T: Copy + num_traits::Num {
-    fn start(&self) -> Point3D<T,UnknownUnit> {
-        self.start
-    }
-
-    fn direction(&self) -> Vector3D<T,UnknownUnit> {
-        self.direction
-    }
-}
-
-impl<T> Ray<T> for HalfLine<T> where T: Copy + num_traits::Num {
-    fn start(&self) -> Point3D<T,UnknownUnit> {
-        self.start
-    }
-
-    fn direction(&self) -> Vector3D<T,UnknownUnit> {
-        self.direction
-    }
-}
-
 impl<T> Intersect<Line<T>,T> for Ball<T> where T: Copy + num_traits::Num + SquareRoot + From<f32> + PartialOrd {
-    // should return the point which is closer to the start of the line and then the farther
-    fn intersections(&self, object: &Line<T>) -> Box<[Point3D<T,UnknownUnit>]> {
+    type Intersection=Point3D<T,UnknownUnit>;
+
+    // should return the point which is closer to the start of the line and then the farther one, also the parameter for the returned point has to be within the parameter bounds
+    fn intersections(&self, object: &Line<T>) -> Box<[Self::Intersection]> {
         // (lx+t*sx-x0)^2 + (ly+t*sy-y0)^2 + (lz+t*sz-z0)^2 = r^2
         // (lx-x0)^2 + 2*(lx-x0)*t*sx + t^2*sx^2  +  (ly-y0)^2 + 2*(ly-y0)*t*sy + t^2*sy^2  +  (lz-z0)^2 + 2*(lz-z0)*t*sz + t^2*sz^2 = r^2
         // (lx-x0)^2 + (ly-y0)^2 + (lz-z0)^2  +  (2*sx*(lx-x0) + 2*sy*(ly-y0) + 2*sz*(lz-z0))*t  +  (sx^2 + sy^2 + sz^2)*t^2 = r^2
@@ -119,50 +93,16 @@ impl<T> Intersect<Line<T>,T> for Ball<T> where T: Copy + num_traits::Num + Squar
             // t1 should be closer to 0 than t2
             let t1 = (T::from(-1.0)*b-discriminant.sqrt())/(T::from(2.0)*a);
             let t2 = (T::from(-1.0)*b+discriminant.sqrt())/(T::from(2.0)*a);
-            let p1 = object.point(t1);
-            let p2 = object.point(t2);
-            Box::new([p1,p2])
-        }else if discriminant==T::zero() {
-            let t = (T::from(-1.0)*b)/(T::from(2.0)*a);
-            let p = object.point(t);
-            Box::new([p])
-        }else{
-            Box::new([])
-        }
-    }
-}
-
-impl<T> Intersect<HalfLine<T>,T> for Ball<T> where T: Copy + num_traits::Num + SquareRoot + From<f32> + PartialOrd {
-    // should return the point which is closer to the start of the line and then the farther, also the parameter for the returned point has to be >=0
-    fn intersections(&self, object: &HalfLine<T>) -> Box<[Point3D<T,UnknownUnit>]> {
-        // (lx+t*sx-x0)^2 + (ly+t*sy-y0)^2 + (lz+t*sz-z0)^2 = r^2
-        // (lx-x0)^2 + 2*(lx-x0)*t*sx + t^2*sx^2  +  (ly-y0)^2 + 2*(ly-y0)*t*sy + t^2*sy^2  +  (lz-z0)^2 + 2*(lz-z0)*t*sz + t^2*sz^2 = r^2
-        // (lx-x0)^2 + (ly-y0)^2 + (lz-z0)^2  +  (2*sx*(lx-x0) + 2*sy*(ly-y0) + 2*sz*(lz-z0))*t  +  (sx^2 + sy^2 + sz^2)*t^2 = r^2
-        // a = sx^2 + sy^2 + sz^2
-        // b = 2*(sx*(lx-x0) + sy*(ly-y0) + sz*(lz-z0))
-        // c = (lx-x0)^2 + (ly-y0)^2 + (lz-z0)^2 - r^2
-        // D = b^2-4*a*c
-        // t12 = (-b+-sqrt(D))/2a
-        let difference = object.start-self.center;
-        let a = object.direction.square_length();
-        let b = T::from(2.0)*(object.direction.dot(difference));
-        let c = difference.square_length()-self.radius*self.radius;
-        let discriminant = b*b-T::from(4.0)*a*c;
-
-        if discriminant>T::zero() {
-            // t1 should be closer to 0 than t2
-            let t1 = (T::from(-1.0)*b-discriminant.sqrt())/(T::from(2.0)*a);
-            let t2 = (T::from(-1.0)*b+discriminant.sqrt())/(T::from(2.0)*a);
-            if t1>=T::zero() {
+            if t1>=object.parameter_bounds.0 && t1 <= object.parameter_bounds.1 {
                 let p1 = object.point(t1);
-                if t2>=T::zero() {
+                if t2>=object.parameter_bounds.0 && t2 <= object.parameter_bounds.1 {
                     let p2 = object.point(t2);
                     Box::new([p1,p2])
                 }else{
                     Box::new([p1])
                 }
             }else{
-                if t2>=T::zero() {
+                if t2>=object.parameter_bounds.0 && t2 <= object.parameter_bounds.1 {
                     let p2 = object.point(t2);
                     Box::new([p2])
                 }else{
@@ -183,50 +123,47 @@ impl<T> Intersect<HalfLine<T>,T> for Ball<T> where T: Copy + num_traits::Num + S
     }
 }
 
+impl<T> Intersect<Ball<T>,T> for Ball<T> where T: Copy + num_traits::Num + SquareRoot + From<f32> + PartialOrd {
+    type Intersection=Circle<T>;
+
+    fn intersections(&self, object: &Ball<T>) -> Box<[Self::Intersection]> {
+        let center_distance = (self.center-object.center).square_length();
+        todo!()
+    }
+}
+
 #[derive(Debug)]
 pub struct Line<T> where T: Copy + num_traits::Num {
     start: Point3D<T,UnknownUnit>,
     direction: Vector3D<T,UnknownUnit>,
+    parameter_bounds: (T,T),
 }
-
-pub struct HalfLine<T> where T: Copy + num_traits::Num {
-    start: Point3D<T,UnknownUnit>,
-    direction: Vector3D<T,UnknownUnit>,
-}
-
-/*
-pub struct HalfLine<T>(Line<T>) where T: Copy + num_traits::Num;
-
-impl<T> Deref for HalfLine<T> where T: Copy + num_traits::Num {
-    type Target=Line<T>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}*/
 
 // (x-x0)^2 + (y-y0)^2 + (z-z0)^2 = r^2
 #[derive(Debug)]
-struct Ball<T> where T: Copy + num_traits::Num {
+pub struct Ball<T> where T: Copy + num_traits::Num {
     center: Point3D<T,UnknownUnit>,
     radius: T,
 }
 
-pub trait Reflect<L,T> where L: Ray<T>, T: Copy + num_traits::Num {
-    fn reflect(&self, line: &L) -> Option<Line<T>>;
+pub struct Circle<T> where T: Copy + num_traits::Num {
+    center: Point3D<T,UnknownUnit>,
+    radius: T,
+    normal: Vector3D<T,UnknownUnit>,
 }
 
-impl<L,T> Reflect<L,T> for Ball<T> where T: Copy + num_traits::Num + SquareRoot + From<f32> + PartialOrd, L: Ray<T>, Ball<T>: Intersect<L,T> {
+impl<T> Reflect<T> for Ball<T> where T: Copy + num_traits::Num + SquareRoot + From<f32> + PartialOrd {
     
-    fn reflect(&self, line: &L) -> Option<Line<T>> {
+    fn reflect(&self, line: &Line<T>) -> Option<Line<T>> {
         let point = self.intersections(line);
         if point.len()>0 {
             let point=point[0];
             let normal = self.normal(point);
-            let reflected = line.direction()-normal*T::from(2.0)*line.direction().dot(normal)/normal.square_length();
+            let reflected = line.direction-normal*T::from(2.0)*line.direction.dot(normal)/normal.square_length();
             Some(Line{
                 start: point,
                 direction: reflected,
+                parameter_bounds: (T::zero(),T::one()/T::zero()),
             })
         }else{
             None
@@ -255,9 +192,10 @@ fn setup(
         ..Default::default()
     });*/
 
-    let line: HalfLine<f32> = HalfLine{
+    let line: Line<f32> = Line{
         direction:(1.0,2.0,-2.0).into(),
         start:(-5.0,-5.0,8.0).into(),
+        parameter_bounds: (0.0,f32::INFINITY),
     };
 
     let ball: Ball<f32> = Ball{
